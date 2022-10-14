eddc1eed6fcc4f350a200064ff37ba921bc1528d
Michael Correa <sales@micomar.com>
2022-10-14 04:04:07 -0400

    api build

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// post all comments
+
+router.post("/", withAuth, (req, res) => {
+    Comment.create({ ...req.body, user_id: req.session.user_id })
+        .then(newComment => {
+            res.json(newComment);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        }
+        );
+});
+
+module.exports = router;
+
+
+
