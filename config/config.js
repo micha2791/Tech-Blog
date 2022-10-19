8de9d59b7f18202af893defd5731ce2038fea382
Michael Correa <sales@micomar.com>
2022-10-18 21:53:58 -0400

    database created

--- config/config.js
+++ config/config.js
@@ -1,26 +1,21 @@
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
-
-// Option 1: Passing parameters separately
+const fs = require('fs')
+const path = require('path')
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PW,
-        {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
-        }
-    );
-    }
+    });
+}
 
+// export the module
 module.exports = sequelize;
 
 
